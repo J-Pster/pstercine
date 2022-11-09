e853bf9545774f54cd4869b8edbf0da0a1899c57
João Pster <joaopsterdev@gmail.com>
2022-11-09 16:58:00 -0300

    refactor(utils): added new utils functions

--- src/utils/Request.js
+++ src/utils/Request.js
@@ -1,17 +1,21 @@
 import axios from 'axios';
 
+const baseImageUrl = 'https://image.tmdb.org/t/p';
+
 const api = axios.create({
   baseURL: 'https://api.themoviedb.org/3/',
 });
 
 export const setHeaderToken = (token) => {
-  api.defaults.headers.common.Authorization = token;
+  api.defaults.headers.common.Authorization = `Bearer ${token}`;
 };
 
 export const requestGet = async (endpoint, params = {}) => {
   const { data } = await api.get(endpoint, { params });
   return data;
 };
+
+export const getImageUrl = (path, size = 'w300') => `${baseImageUrl}/${size}${path}`;
 
 export const requestPost = async (endpoint, body) => {
   const { data } = await api.post(endpoint, body);
